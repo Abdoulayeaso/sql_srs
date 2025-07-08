c6b37a3360f8b83c4a96e8acd44867f13cf8f366
abdoulayeaso <ssoumah974@gmail.com>
2025-07-08 16:31:00 +0200

    add sidebar

--- app.py
+++ app.py
@@ -8,24 +8,27 @@ st.write(""""
 Spaced Repetition System SQL practice
 """)
 
-option = st.selectbox(
-    "What do you like to review?",
-    ("Joins", "GroupBy", "Windows Functions"),
-    index=None,
-    placeholder="Select a theme...",
-    )
+with st.sidebar:
+    option = st.selectbox(
+        "What do you like to review?",
+        ("Joins", "GroupBy", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme...",
+        )
 
-st.write('You selected:', option)
+    st.write('You selected:', option)
 
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
-tab1, tab2, tab3, tab4 = st.tabs(["Cat", "Dog", "Owl", "txt"])
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    sql_query = st.text_area(label="entrez votre input")
+    result = duckdb.query(sql_query)
+    st.write(f"vous avez entré la query suivante: {sql_query}")
+    st.dataframe(result)
 
 with tab2:
     st.header("A dog")
@@ -35,9 +38,5 @@ with tab3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
-with tab4:
-    sql_query = st.text_area(label="entrez votre input")
-    result = duckdb.query(sql_query)
-    st.write(f"vous avez entré la query suivante: {sql_query}")
-    st.dataframe(result)
+
 
