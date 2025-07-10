c24ea484c8ee02088fe255e272682b4ace9defef
abdoulayeaso <ssoumah974@gmail.com>
2025-07-11 00:35:31 +0200

    init db on st

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 # pylint: disable=missing-module-docstring
-import ast
+
+import os
+import logging
 import duckdb
 import streamlit as st
+from datetime import date, timedelta
+
+
+if "data" not in os.listdir():
+    print("creating folder data")
+    logging.error(os.listdir())
+    logging.error("creating folder data")
+    os.mkdir("data")
+
+if "exercises_sql_tables.duckdb" not in os.listdir("data"):
+    exec(open("init_db.py").read())
+    # subprocess.run(["python", "init_db.py"])
 
 con = duckdb.connect(database="data/excercises_sql_tables.duckdb", read_only=False)
 
@@ -14,7 +28,7 @@ with st.sidebar:
     )
     st.write("You selected:", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df().sort_values("last_reviewed").reset_index()
     st.write(exercise)
     exercise_name = exercise.loc[0, "exercise_name"]
     with open(f"answers/{exercise_name}.sql", "r") as f:
@@ -43,7 +57,7 @@ if query:
 tab2, tab3 = st.tabs(["tables", "Solution"])
 
 with tab2:
-    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    exercise_tables = exercise.loc[0, "tables"]
     for table in exercise_tables:
         st.write(f"table: {table}")
         df_table = con.execute(f"SELECT * FROM {table}").df()
